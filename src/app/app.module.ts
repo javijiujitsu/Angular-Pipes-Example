14cfd2b6ad4088ddec7c655a54d4100cdfdf635b
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-04 15:36:41 -0400

    added a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
+import { CustomPipesComponent } from './custom-pipes/custom-pipes.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuiltInPipesComponent
+    BuiltInPipesComponent,
+    CustomPipesComponent,
+    CapitalizePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+     HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
